8d0db5798040ba96e550cecd23fb9b4252487dd3
Truong Duong Mau <truongduongmau@gmail.com>
2024-11-02 21:53:55 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@ import { useCallback, useState } from "react";
 import Home from "./home/page";
 
 export default async function App() {
+  const apiUrl = process.env.API_URL;
+  const localhost = process.env.LOCAL_HOST
   const salt = (new Date()).getTime();
-  
+  const page = 1;
+
   const url = `${apiUrl}/doc-nhanh/trang-${page}.chn?${salt}`;
-  const data = await fetch(`http://localhost:3000/api?url=${url}`, { cache: 'no-store' })
+  const data = await fetch(`${localhost}/api?url=${url}`, { cache: 'no-store' })
   const content = await data.text()
 
   return (
