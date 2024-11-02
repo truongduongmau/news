2d57e86eb7aab3b6ccc0169ab3947291ae54b5eb
Truong Duong Mau <truongduongmau@gmail.com>
2024-11-02 23:33:52 +0700

    update

--- src/app/components/home/home.tsx
+++ src/app/components/home/home.tsx
@@ -1,17 +1,4 @@
 import HomeMain from "./home-main";
-import { loadPosts } from '../../lib/redirect'
-
-export async function getStaticProps() {
-  const apiUrl = process.env.API_URL;
-  const localhost = process.env.LOCAL_HOST
-  const salt = (new Date()).getTime();
-  const page = 1;
-
-  const url = `${apiUrl}/doc-nhanh/trang-${page}.chn?${salt}`;
-  const data = await loadPosts(url)
-
-  return await data.text();
-}
 
 const Home = async () => {
   const apiUrl = process.env.API_URL;
@@ -20,9 +7,12 @@ const Home = async () => {
   const page = 1;
 
   const url = `${apiUrl}/doc-nhanh/trang-${page}.chn?${salt}`;
-  const data = await fetch(`${localhost}/api?url=${url}`, { cache: 'no-store' })
+  //const data = await fetch(`${localhost}/api?url=${url}`, { cache: 'no-store' })
   //const content = await data.text();
-  const content = await getStaticProps();
+
+  const data = await fetch(url)
+  const content = await data.text();
+
   return (
     <>
       <HomeMain content={content}></HomeMain>
