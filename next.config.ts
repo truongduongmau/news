8e9ab9727b0e71eefca7cd0a0e178a448973026e
Trương Dương Mậu <truongduongmau@gmail.com>
2024-11-02 10:09:14 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: './docs',
+  basePath: "",
+  output: "export",
 };
 
 export default nextConfig;
