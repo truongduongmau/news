5ac9419ad32b9849a97a0beda97f456d67e28738
Trương Dương Mậu <truongduongmau@gmail.com>
2024-11-02 13:27:43 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default async function Home() {
-  const data = await fetch('http://localhost:3000/api')
+  const data = await fetch('https://truongduongmau.github.io/news/api')
   const posts = await data.json()
   
   return (
