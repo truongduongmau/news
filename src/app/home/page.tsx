eeb7066f676c77b205a5bf8175d0ef8e090cd0a9
Trương Dương Mậu <truongduongmau@gmail.com>
2024-11-02 17:45:34 +0700

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useRef } from "react";
+import { ReactElement, ReactNode, useEffect, useRef } from "react";
 import useIsServer from "../useHook/useIsServer";
 
 const Home = ({ content }) => {
@@ -8,21 +8,46 @@ const Home = ({ content }) => {
   const fastNews = useRef<HTMLDivElement>(null)
   if (isServer) return (<></>)
 
+  interface IOptions {
+    options: string[]
+  }
+
+  const CardArray: React.FC<IOptions> = ({ options }) => {
+    return <>{options.map(opt => opt)}</>
+  }
+
   const dom_document = new DOMParser().parseFromString(content, "text/html");
   const news: NodeListOf<Element> = dom_document.querySelectorAll("div.list-fast-news > .item") || [];
-  debugger
-  //const list = Array.apply(null, Array(news.length)).map((v, key) => news[key])
-  //const items = list.map((item, key) => fastNews.current = item)
 
-  //fastNews.current. = news[0] as HTMLDivElement
+  const list = Array.apply(null, Array(news.length)).map((v, key) => news[key])
+  const items = list.map((item, key) => {
+    const time = item.querySelector("span.time");
+    const timeText = (time?.textContent || "").replace("(", "").replace(")", "");
+    const title = item.querySelector(".title-wrap > a")?.textContent
+    const href = item.querySelector(".title-wrap > a")?.getAttribute("href")?.toString();
+    const content = item.querySelector(".nv-details > .abs")?.textContent
+    return <div className="item expandable active">
+      <div className="nv-text-cont">
+        <div className="timeTitle time-ago" title={timeText}>{timeText}</div>
+        <div className="title-wrap">
+          <a target="_blank" className="news-title" href={href} title="{title}">{title}</a>
+        </div>
+      </div>
+      <div className="nv-details">
+        <span className="time">(02/11/2024 17:19)</span>
+        <div className="abs">{content}</div>
+        <div className="text-right">
+          <a target="_blank" className="doctoanbo" href="/bat-dong-san-tpthu-duc-tang-toc-cuoi-nam-nho-vanh-dai-2-sap-khep-kin-18824110217083289.chn" title="Đọc toàn bộ">Đọc toàn bộ</a>
+        </div>
+      </div>
+    </div>
+  })
 
-  news.forEach((item, key) => {document.getElementById("fastNews")?.append(item) })
-
-return (
-  <>
-    <div id="fastNews"></div>
-  </>
-);
+  return (
+    <>
+      {items}
+    </>
+  );
 }
 
 export default Home
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,31 @@ export default async function App() {
 
   return (
     <>
-      <Home content={content} />
+      <div className="fastnews-main">
+        <h1 className="tinnhanh_title">
+          <span>Tin mới cập nhật</span>
+        </h1>
+        <div className="nviewer" id="fastnews-main-contents">
+          <Home content={content} />
+        </div>
+        <div className="loadingio-spinner-spinner-l1xppaoi24">
+          <div className="ldio-jkw3q3bi0aa">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="buttons"><button className="card card-small">Đọc thêm</button></div>
+      </div>
     </>
   );
 }
